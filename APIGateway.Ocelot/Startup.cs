f2c524193b8a6d13623ee4760d9f8193f0359cdf
磊 孙 <77540975@qq.com>
2020-11-10 17:58:53 +0800

    Added gravel app test serv.

--- APIGateway.Ocelot/Startup.cs
+++ APIGateway.Ocelot/Startup.cs
@@ -27,8 +27,9 @@ namespace APIGateway.Ocelot
         public void ConfigureServices(IServiceCollection services)
         {
             // IssuerSigningKey
-            Console.WriteLine("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhcGlnYXRld2F5LmF1ZCIsImlzcyI6ImFwaWdhdGV3YXkuaXNzIiwiZXhwIjoxNjM2MDkzODYwLCJJc0Z1a2luVXNlciI6InllcyBpJ20gZnVja2luIHVzZXIifQ." +
-            Convert.ToBase64String((new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration["TokenManagement:Secret"]))).Key));
+            // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhcGlnYXRld2F5LmF1ZCIsImlzcyI6ImFwaWdhdGV3YXkuaXNzIiwiZXhwIjoxNjM2NTMxNTczLCJJc0Z1a2luVXNlciI6InllcyBpJ20gZnVja2luIHVzZXIifQ.W5mCBrPUYc1gx1L-HiUqxH2h0eJBoS5uEB5EQIXVO-0
+            // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJHcmF2ZWwuQXBwU2VjcmV0QXVkaWVuY2UiLCJpc3MiOiJHcmF2ZWwuQXBwSXNzdWVyIiwiZXhwIjoxNjM2NTMxNTczLCJTdXBwbGllclVuaXRJZCI6IkVFN0IzREFBLTE3RDItNDMzMS05RjAwLTY5NzEwMTgwRjhGNSJ9.Tqu1bw_rxyfCdwq9ZErb-AQm4Z9SbG36HDpY9iH_W1U
+            Console.WriteLine(Convert.ToBase64String((new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration["TokenGravelApp:Secret"]))).Key));
 
             services.AddAuthentication()
             .AddJwtBearer("scetiJwtAuth", jbo =>
@@ -43,6 +44,21 @@ namespace APIGateway.Ocelot
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration["TokenManagement:Secret"])),
                     ValidIssuer = Configuration["TokenManagement:Issuer"],
                     ValidAudience = Configuration["TokenManagement:Audience"],
+
+                };
+            })
+             .AddJwtBearer("Gravel.App", jbo =>
+            {
+                jbo.RequireHttpsMetadata = false;
+                jbo.SaveToken = true;
+                jbo.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration["TokenGravelApp:Secret"])),
+                    ValidIssuer = Configuration["TokenGravelApp:Issuer"],
+                    ValidAudience = Configuration["TokenGravelApp:Audience"],
 
                 };
             });
